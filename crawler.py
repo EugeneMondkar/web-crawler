8adaa267bf1ca22d3ea23de9d0cf20df9368dd20
Eugene Mondkar <yatinm@gmail.com>
2021-09-28 12:12:23 -0700

    imported and tested BeautifulSoup
    now able to request and process web pages

--- crawler.py
+++ crawler.py
@@ -3,13 +3,19 @@
 # 
 # This module holds the functions and classes required for web crawling
 
-import urllib
+import urllib.request as urlreq
+from bs4 import BeautifulSoup
 
 seed = "https://www.mtsac.edu/"
 
 frontier = []
 
-html_reader = urllib.urlopen(seed)
+html_reader = urlreq.urlopen(seed)
 
+# Test to see if the URL request is working
+# print(html_reader.read())
 
+# Creating BeautifulSoup object
+bs_obj = BeautifulSoup(html_reader)
 
+print(bs_obj.h1)

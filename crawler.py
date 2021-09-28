181a6ad114ce647c05ef8d773676004c98411f3c
Eugene Mondkar <yatinm@gmail.com>
2021-09-28 14:02:36 -0700

    switched to httplib2 for making requests

--- crawler.py
+++ crawler.py
@@ -9,28 +9,42 @@
 # TODO: Process webpages and extract information for document store
 # TODO: Decide on file format for webpage content in document store
 
-import urllib.request as urlreq
-from urllib.error import HTTPError, URLError
 
+
+import httplib2
 from bs4 import BeautifulSoup, SoupStrainer
 
 seed = "https://www.mtsac.edu/"
 
 frontier = []
 
-try:
-    html_reader = urlreq.urlopen(seed)
-except HTTPError as e:
-    print(e)
-except URLError as e:
-    print(e, "Server could not be found")
-else:
-    print("Something went wrong") 
+http_obj = httplib2.Http()
+
+status, response = http_obj.request(seed)
+
+for link in BeautifulSoup(response, parse_only=SoupStrainer('a')):
+    if link.has_attr('href'):
+        print(link['href'])
+
+
+# Code using urllib modules
+
+# import urllib.request as urlreq
+# from urllib.error import HTTPError, URLError
+
+# try:
+#     html_reader = urlreq.urlopen(seed)
+# except HTTPError as e:
+#     print(e)
+# except URLError as e:
+#     print(e, "Server could not be found")
+# else:
+#     print("Something went wrong") 
 
 # Test to see if the URL request is working
 # print(html_reader.read())
 
 # Creating BeautifulSoup object
-bs_obj = BeautifulSoup(html_reader)
+# bs_obj = BeautifulSoup(html_reader)
 
-print(bs_obj)
+# print(bs_obj)

5d05940c56dea047c0fb9a8d232be355d697c1a8
Eugene Mondkar <yatinm@gmail.com>
2021-09-28 14:18:50 -0700

    Tested and validated link extraction algo

--- crawler.py
+++ crawler.py
@@ -3,31 +3,53 @@
 # 
 # This module holds the functions and classes required for web crawling
 #
-# TODO: Decide on HTTP request library: urllib or httplib2
-# TODO: Retrieve all links and store in frontier
+# DONE: Decide on HTTP request library: urllib or httplib2
+# DONE: Retrieve all links and store in frontier
+# TODO: Encapsulate Link Extraction
 # TODO: Iterate over the frontier and decide on crawl algo
 # TODO: Process webpages and extract information for document store
 # TODO: Decide on file format for webpage content in document store
+# TODO: Handle HTTP error codes
 
 
 
 import httplib2
 from bs4 import BeautifulSoup, SoupStrainer
 
-seed = "https://www.mtsac.edu/"
+if __name__ == '__main__':
 
-frontier = []
+    seed = "https://www.mtsac.edu"
 
-http_obj = httplib2.Http()
+    frontier = []
 
-status, response = http_obj.request(seed)
+    http_obj = httplib2.Http()
 
-for link in BeautifulSoup(response, parse_only=SoupStrainer('a')):
-    if link.has_attr('href'):
-        print(link['href'])
+    # encapsulate into function
+    url = seed # function argument
+    # additional function arguments: http object from httplib2.Http(), and frontier
+    status, response = http_obj.request(url)
+
+    for link in BeautifulSoup(response, parse_only=SoupStrainer('a'), features='html.parser'):
+        if link.has_attr('href'):
+            if link['href'][0] == '/':
+                extracted_url = url + link['href']
+            elif link['href'][:4] == 'http':
+                extracted_url = link['href']
+            
+            frontier.append(extracted_url)
+
+    print('Stored Links:')
+    for i, link in enumerate(frontier):
+        print(i, link)
+
+    # end of function
 
 
-# Code using urllib modules
+
+
+
+
+## Code using urllib modules ##
 
 # import urllib.request as urlreq
 # from urllib.error import HTTPError, URLError

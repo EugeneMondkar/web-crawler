5c3d75692b3eb0b1fc31cb6e9e50fb47a95f1b65
Eugene Mondkar <yatinm@gmail.com>
2021-09-28 11:56:17 -0700

    created crawler module
    crawler module contains functions and classes necessary for web crawling

---
+++ crawler.py
@@ -0,0 +1,15 @@
+# Author: Eugene Mondkar
+# Group 10
+# 
+# This module holds the functions and classes required for web crawling
+
+import urllib
+
+seed = "https://www.mtsac.edu/"
+
+frontier = []
+
+html_reader = urllib.urlopen(seed)
+
+
+

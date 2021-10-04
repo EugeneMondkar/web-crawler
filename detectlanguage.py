412a6a9d62e4b53eda14c5695aca6e2b0b484d16
RachelGoodrich <71162103+RachelGoodrich@users.noreply.github.com>
2021-10-03 23:32:57 -0700

    added function detect_and_create
    - Added function detect_and_create which creates directory for the text files
    - Edited previous functions so they now assume a full file path is being provided from detect_and_create

--- detectlanguage.py
+++ detectlanguage.py
@@ -1,18 +1,18 @@
 from langdetect import detect
 from bs4 import BeautifulSoup
+import os
 
 # There are two functions in this file: detect_language and create_text_file. 
 # detect_language can be used inside the crawler to ensure that only the webpages in the correct language are stored
 
-def detect_language(fileName, repository_path):
+def detect_language(fileName):
 
 #This looks at the first paragraph to detect the language
 #It returns a 1 in english, 2 if german, 3 if spanish and 0 if other.
 #if detect_language returns less than 1 or not the intended language, then do not continue using that link.
 
     #open up the file
-  fullFileName = repository_path + fileName
-  html = open(fullFileName, "r")
+  html = open(fileName, "r")
   soup = BeautifulSoup(html, 'html.parser')
 
   # The first paragraph may be empty space, so a loop is used to iterate through until a paragraph without empty space is found
@@ -42,12 +42,11 @@ def detect_language(fileName, repository_path):
     print("Language detected: Other")
     return 0
 
-def create_text_file(fileName, repository_path, language_num):
+def create_text_file(fileName, text_files_path, language_num):
 #This fully parses the html file and creates/appends the text to a file depending on which language is given.
 
   #open up the file
-  fullFileName = repository_path + fileName
-  html = open(fullFileName, "r")
+  html = open(fileName, "r")
   soup = BeautifulSoup(html, 'html.parser')
   document = ""
 
@@ -64,7 +63,7 @@ def create_text_file(fileName, repository_path, language_num):
   else:
     htmlFileName = "other.txt"
 
-  fullFileName = repository_path + htmlFileName
+  fullFileName = text_files_path + htmlFileName
 
   # write text to a text file
   print("Writing to file...")
@@ -74,13 +73,12 @@ def create_text_file(fileName, repository_path, language_num):
   f.close()
   print("finished!")
 	
-def detect_language_exhaustive(fileName, repository_path):
+def detect_language_exhaustive(fileName):
 	
 #This function detects the language by looking at all of the text inside of the html, instead of just the first paragraph like detect_language
 
   #open up the file
-  fullFileName = repository_path + fileName
-  html = open(fullFileName, "r")
+  html = open(fileName, "r")
   soup = BeautifulSoup(html, 'html.parser')
   document = ""
 
@@ -106,3 +104,24 @@ def detect_language_exhaustive(fileName, repository_path):
   else:
     print("Language detected: Other")
     return 0
+
+def detect_and_create(text_files_path, repository_path, num_of_files):
+# this function creates the directory and calls the other functions so that language detection is done on every file in the repository. It takes in the desired path of the text file directiory, the repository path, and the number of files in the repository.
+
+  directory_exists = os.path.isdir(text_files_path)
+  if not directory_exists:
+    print("Creating text file directory...")
+    os.mkdir(text_files_path)
+
+  print(text_files_path)
+  num_of_files = int(num_of_files)
+  for current_html_number in range(num_of_files):
+
+    html_file_name = "{:04d}".format(current_html_number) + "_html_file.html"
+    print(html_file_name)
+    full_path_name = repository_path + html_file_name
+    print(full_path_name)
+    languageNum = detect_language_exhaustive(full_path_name)
+    create_text_file(full_path_name, text_files_path, languageNum)
+
+  print("finished!")

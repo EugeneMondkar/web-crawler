42b776863443f4110233726f9aac2df0d345ae28
RachelGoodrich <rachelegoodrich@gmail.com>
2021-10-05 15:42:12 -0700

    added detect_and_create to main.

--- main.py
+++ main.py
@@ -6,6 +6,9 @@
 
 from crawler import http_crawler # Eugene's Component
 from writer import write_csv # Emily' Component
+from detectlanguage import detect_and_create # Rachel's component
+
+detect_and_create('.\\textfiles\\', '.\\repository\\', 40)
 #######################
 # Add glue code below #
 #######################

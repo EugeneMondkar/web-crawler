9c81617348280f6214585e8060779e8de8974093
EugeneMondkar <yatinm@gmail.com>
2021-10-05 18:21:51 -0700

    Initializing consolidation process

--- main.py
+++ main.py
@@ -8,9 +8,22 @@ from crawler import http_crawler # Eugene's Component
 from writer import write_csv # Emily' Component
 from detectlanguage import detect_and_create # Rachel's component
 
-detect_and_create('.\\textfiles\\', '.\\repository\\', 40)
 #######################
 # Add glue code below #
 #######################
 
+# seed_01 = "https://www.mtsac.edu/"
+
+# seeds = [seed_01]
+
+# crawl_limit = 5
+
+# html_file_path = '.\\repository\\html_files\\'
+
+# text_file_path = '.\\repository\\text_files\\'
+
+# sites_and_outlinks = http_crawler(seeds, crawl_limit, repository_path)
+
+
+# detect_and_create(text_file_path, repository_path, 40)
 
